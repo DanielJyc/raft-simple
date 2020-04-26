820a2c312a18ee08af70bd409a0211702222e08d
DanielJyc <496830205@qq.com>
2020-04-26 22:22:57 +0800

    fix： 优化启动顺序

--- raft-biz-service-impl/src/main/java/top/datadriven/raft/biz/service/impl/component/RaftStarter.java
+++ raft-biz-service-impl/src/main/java/top/datadriven/raft/biz/service/impl/component/RaftStarter.java
@@ -1,16 +1,17 @@
-package top.datadriven.raft.biz.service.impl.component.impl;
+package top.datadriven.raft.biz.service.impl.component;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import top.datadriven.raft.biz.service.impl.component.StartRaftComponent;
+import top.datadriven.raft.biz.service.impl.provider.DubboServiceRegister;
 import top.datadriven.raft.core.model.exception.RaftException;
 import top.datadriven.raft.core.service.handler.StateMachineHandler;
 import top.datadriven.raft.core.service.transformer.ServerStateTransformerStarter;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 
 /**
- * @description: raft 核心组件
+ * @description: raft 启动器
  * @author: jiayancheng
  * @email: jiayancheng@foxmail.com
  * @datetime: 2020/4/18 5:36 下午
@@ -18,7 +19,7 @@ import javax.annotation.Resource;
  */
 @Slf4j
 @Component
-public class StartRaftComponentImpl implements StartRaftComponent {
+public class RaftStarter {
 
     @Resource
     private ServerStateTransformerStarter serverStateTransformerStarter;
@@ -26,19 +27,27 @@ public class StartRaftComponentImpl implements StartRaftComponent {
     @Resource
     private StateMachineHandler stateMachineHandler;
 
-    @Override
+    @Resource
+    private DubboServiceRegister dubboServiceRegister;
+
+    /**
+     * 这里的启动有先后顺序
+     */
+    @PostConstruct
     public void start() {
         try {
-            //1. 启动server 状态流转
+            //1. 启动dubbo服务
+            dubboServiceRegister.registry();
+
+            //2. 启动server 状态流转
             serverStateTransformerStarter.start();
 
-            //2. 启动状态机
+            //3. 启动状态机
             stateMachineHandler.commit2Apply();
         } catch (RaftException raftException) {
             log.error(raftException.getErrorMsg(), raftException);
         } catch (Throwable t) {
             log.error(t.getMessage(), t);
         }
-
     }
 }
